79b30b91847e8745646d73908ae3754b09147d03
Travis Hornbuckle <razorrussian@gmail.com>
2026-03-31 20:07:40 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rxrrssn.github.io/conference",
-  //base: "/rxrrssn.github.io",
+  site: "https://rxrrssn.github.io/",
+  base: "/conference",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
